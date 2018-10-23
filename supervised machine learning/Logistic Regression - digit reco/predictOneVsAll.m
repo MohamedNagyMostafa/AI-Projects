c2ad554301345b48ccd80b733a6973e1019a42ed
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2018-10-23 21:48:42 +0200

    thetas values

--- supervised machine learning/Logistic Regression - digit reco/predictOneVsAll.m
+++ supervised machine learning/Logistic Regression - digit reco/predictOneVsAll.m
@@ -1,3 +1,5 @@
 function p = predictOneVsAll(theta, X)
+  
+   X =[0 X] 
   [a, p] = max(logistic(X*theta'),[],2)
 endfunction
--- supervised machine learning/Logistic Regression - digit reco/trainingOneVsAll.m
+++ supervised machine learning/Logistic Regression - digit reco/trainingOneVsAll.m
@@ -1,7 +1,7 @@
 function thetas = trainingOneVsAll(X, y, lambda)
   num_labels = 10;
   [m n] = size(X);
-  options = optimset('GradObj', 'on', 'MaxIter', 50);
+  options = optimset('GradObj', 'on', 'MaxIter', 10);
 
   for c = 1:num_labels
     theta_initial = zeros(n+1, 1);
