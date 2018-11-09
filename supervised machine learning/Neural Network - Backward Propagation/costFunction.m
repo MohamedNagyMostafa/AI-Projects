974b4c4ab1c32b292b4bf4d07b9a27c025b4cd42
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2018-11-09 18:20:33 +0200

    Neural Network Backward propagation --feed backward--

--- supervised machine learning/Neural Network - Backward Propagation/costFunction.m
+++ supervised machine learning/Neural Network - Backward Propagation/costFunction.m
@@ -23,5 +23,26 @@ function [J grad] = costFunction(inputLayers, hiddenLayers, outputLayers, Theta,
   p = (lambda./2.0) .* sum([Theta1(:,2:end)(:);Theta2(:,2:end)(:)].^2);
   J = (-sum(sum(Y .* log(a3) + (1-Y) .* log(1-a3),2),1)+ p)./num_examples;
   
+  %Feed Backward ...
+  deltas_3 = (a3 - Y)';
+  deltas_2 = (deltas_3' * Theta2(:,2:end) .* sigmoidPrime(z2))';
+  
+  delta_2 = deltas_3 * a2;
+  delta_1 = deltas_2 * a1;
+  
+  D_1 = zeros(size(Theta1));
+  D_2 = zeros(size(Theta2));
+  
+  %Determine Bias Units ...
+  %As the backward propagation will be repeated for one time
+  %and the initial delta is 0 then the first equation of 
+  
+  D_1(:,2:end) =  (delta_1(:,2:end) + lambda .* Theta1(:,2:end))./num_examples;
+  D_2(:,2:end) =  (delta_2(:,2:end) + lambda .* Theta2(:,2:end))./num_examples;
+  D_1(:,1) = delta_1(:,1)./num_examples;
+  D_2(:,1) = delta_2(:,1)./num_examples;
+  
+  
+  grad = [D_1(:);D_2(:)];
   
 endfunction
